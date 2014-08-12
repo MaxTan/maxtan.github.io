bb1079fcd953f7e85dcbe4b54d8b5cf3c6e9c691
MaxTan <763305825@qq.com>
2014-08-12 20:54:41 +0800

    测试博文

--- meta.js
+++ meta.js
@@ -1,17 +1,35 @@
 {
-    "site_name": "小天天博客",
-    "copyright": "@2014 By Max",
-    "cates": [
-    {"name": "tech", "text":"技术"},
-    {"name": "art", "text":"艺术"},
-    {"name": "other", "text":"其它"}
-    ],
-    "articles":[
-    {"title": "tech hello world", "file": "2012-01-23", "cate":"tech"},
-    {"title": "art hello world", "file": "2012-02-23", "cate":"art"},
-    {"title": "other hello world", "file": "2012-03-23", "cate":"other"}
-    ],
-    "blogs":[
-        {"title":"test Blog!!","file":"2012-03-23","desc":"随便填些内容.........."}
-    ]
+  "site_name": "快到屌炸天的博客",
+  "copyright": "@2014 By Max",
+  "cates": [
+    {
+      "name": "tech",
+      "text": "技术"
+    },
+    {
+      "name": "art",
+      "text": "扯淡"
+    },
+    {
+      "name": "other",
+      "text": "其它"
+    }
+  ],
+  "articles": [
+    {
+      "title": "tech hello world",
+      "file": "2012-01-23",
+      "cate": "tech"
+    },
+    {
+      "title": "art hello world",
+      "file": "2012-02-23",
+      "cate": "art"
+    },
+    {
+      "title": "other hello world",
+      "file": "2012-03-23",
+      "cate": "other"
+    }
+  ]
 }
