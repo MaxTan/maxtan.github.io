7b821a4df0535ca56857870073e90f27182617aa
tanyunxing <tanyunxing@gdepb.gov.cn>
2014-08-12 17:37:53 +0800

    使用javascript全静态博客

---
+++ meta.js
@@ -0,0 +1,17 @@
+{
+    "site_name": "小天天博客",
+    "copyright": "@2014 By Max",
+    "cates": [
+    {"name": "tech", "text":"技术"},
+    {"name": "art", "text":"艺术"},
+    {"name": "other", "text":"其它"}
+    ],
+    "articles":[
+    {"title": "tech hello world", "file": "2012-01-23", "cate":"tech"},
+    {"title": "art hello world", "file": "2012-02-23", "cate":"art"},
+    {"title": "other hello world", "file": "2012-03-23", "cate":"other"}
+    ],
+    "blogs":[
+        {"title":"test Blog!!","file":"2012-03-23","desc":"随便填些内容.........."}
+    ]
+}
